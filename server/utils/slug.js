8b508f855f333bc4582d1d0dbc35ae8ca1eafdcb
Chijioke Uzodinma <embroconnect3@gmail.com>
2026-03-11 00:22:40 +0100

    feat: implement cloudinary uploader utility

---
+++ server/utils/slug.js
@@ -0,0 +1,9 @@
+export const slugify = (name) => {
+    return name
+        .toString()
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+        .replace(/\-\-+/g, '-') // Replace multiple - with single -
+}
